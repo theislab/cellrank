a5eefc7da7580e324f5fe2d41fb447bbf5eb1ac1
Michal Klein <michal.klein@tngtech.com>
2020-03-20 13:23:40 +0100

    Add proper docstring for find_root and find_final

--- cellrank/tools/_root_final.py
+++ cellrank/tools/_root_final.py
@@ -9,22 +9,20 @@ from cellrank.tools._transition_matrix import transition_matrix
 from cellrank.utils._docs import inject_docs
 
 _find_docs = """\
-Root cells and final cells of a dynamic process in single cells.
-
-Computes root and final cells based on RNA velocity, see [Manno18]_. The tool models dynamic cellular
+Computes {cells} cells based on RNA velocity, see [Manno18]_. The tool models dynamic cellular
 processes as a Markov chain, where the transition matrix is computed based on the velocity vectors of each
 individual cell. The spectrum of the transition matrix can be used to query approximate recurrent classes of the
-Markov chain, which represent groups of root- or final cells.
+Markov chain, which represent groups of {cells} cells.
 
 Cells are filtered into transient/recurrent cells using the left eigenvectors of the transition matrix and clustered
-into distinct groups of root or final cells using the right eigenvectors of the transition matrix of the Markov chain.
+into distinct groups of {cells} cells using the right eigenvectors of the transition matrix of the Markov chain.
 
 Params
 ------
 adata : :class:`adata.AnnData`
     Annotated data object.
 cluster_key
-    The tool can match computed start/end-points against pre-computed clusters to annotate the start/end-points.
+    The tool can match computed {direction}points against pre-computed clusters to annotate the {direction}points.
     For this, provide a key from :paramref:`adata` `.obs` where cluster labels have been computed.
 weight_connectivities
     Weight given to a transition matrix computed on the basis of the KNN connectivities. Should be in `[0, 1]`. This
@@ -33,9 +31,9 @@ percentile
     When making a distinction between transient and recurrent cells, a percentile is used for filtering. Choose
     this value according to the percentage of transient cells you expect to see in your data.
     E.g. :paramref:`percentile` `=98` means you are expecting 98% of your cells to be transient
-    and 2% to be recurrent (start/end-points).
+    and 2% to be recurrent {direction}points.
 n_start_end
-    If you know how many start/end-points you are expecting, you can provide this number.
+    If you know how many {direction}points you are expecting, you can provide this number.
     Otherwise, an eigen-gap heuristic is used.
 show_plots
     Whether to show plots of the spectrum and eigenvectors in the embedding.
@@ -46,7 +44,7 @@ Returns
 -------
 :class:`anndata.AnnData` or :class:`NoneType`
     Depending on :paramref:`copy`, either updates the existing :paramref:`adata` object or returns a copy.
-    Marked cells can be found in :paramref:`adata` `.obs` under `{!r}`.
+    Marked cells can be found in :paramref:`adata` `.obs` under `{key_added!r}`.
 """
 
 
@@ -92,7 +90,9 @@ def _root_final(
     return adata if copy else None
 
 
-@inject_docs(_find_docs.format("root_cells"))
+@inject_docs(
+    root=_find_docs.format(cells="root", direction="start", key_added="root_cells")
+)
 def find_root(
     adata: AnnData,
     cluster_key: Optional[str] = None,
@@ -102,6 +102,12 @@ def find_root(
     show_plots=False,
     copy: bool = False,
 ) -> Optional[AnnData]:
+    """
+    Root cells of a dynamic process in single cells.
+
+    {root}
+    """
+
     return _root_final(
         adata,
         final=False,
@@ -114,7 +120,9 @@ def find_root(
     )
 
 
-@inject_docs(_find_docs.format("final_cells"))
+@inject_docs(
+    final=_find_docs.format(cells="final", direction="end", key_added="final_cells")
+)
 def find_final(
     adata: AnnData,
     cluster_key: Optional[str] = None,
@@ -124,6 +132,12 @@ def find_final(
     show_plots=False,
     copy: bool = False,
 ) -> Optional[AnnData]:
+    """
+    Final cells of a dynamic process in single cells.
+
+    {final}
+    """
+
     return _root_final(
         adata,
         final=True,
