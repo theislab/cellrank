0aa1d60afbdaa1bf219ebacd670c28bbd830b67a
Michal Klein <michal.klein@tngtech.com>
2020-03-23 13:29:24 +0100

    Add pancreas dataset

--- cellrank/__init__.py
+++ cellrank/__init__.py
@@ -2,5 +2,6 @@
 import cellrank.tools as tl
 import cellrank.plotting as pl
 import cellrank.utils as ul
+import cellrank.datasets
 
 from scanpy import settings
---
+++ cellrank/datasets/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+from cellrank.datasets._datasets import pancreas
