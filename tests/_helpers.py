e5758a90fa5cfff0fd7d72b3fa672357e6126627
Michal Klein <michal.klein@tngtech.com>
2020-03-28 10:35:58 +0100

    Switch kernels to pytest

--- tests/_helpers.py
+++ tests/_helpers.py
@@ -10,22 +10,9 @@ from typing import Optional
 from anndata import AnnData
 from scanpy import logging as logg
 
+import pytest
 import numpy as np
 import scvelo as scv
-
-
-def create_dummy_adata(n_obs: int = 50):
-    adata = scv.datasets.toy_data(n_obs=n_obs)
-    scv.pp.filter_and_normalize(adata, min_shared_counts=20, n_top_genes=1000)
-    scv.pp.moments(adata, n_pcs=30, n_neighbors=30)
-    scv.tl.recover_dynamics(adata)
-    scv.tl.velocity(adata, mode="dynamical")
-    scv.tl.velocity_graph(adata)
-    scv.tl.latent_time(adata)
-    adata.uns["connectivity_variances"] = np.ones((50, 50), dtype=np.float64)
-    adata.uns["velocity_variances"] = np.ones((50, 50), dtype=np.float64)
-
-    return adata
 
 
 def bias_knn(conn, pseudotime, n_neighbors, k=3):
@@ -277,6 +264,20 @@ def _is_connected(c):
     G = nx.from_scipy_sparse_matrix(c) if issparse(c) else nx.from_numpy_array(c)
 
     return nx.is_connected(G)
+
+
+def create_dummy_adata(n_obs: int = 50):
+    adata = scv.datasets.toy_data(n_obs=n_obs)
+    scv.pp.filter_and_normalize(adata, min_shared_counts=20, n_top_genes=1000)
+    scv.pp.moments(adata, n_pcs=30, n_neighbors=30)
+    scv.tl.recover_dynamics(adata)
+    scv.tl.velocity(adata, mode="dynamical")
+    scv.tl.velocity_graph(adata)
+    scv.tl.latent_time(adata)
+    adata.uns["connectivity_variances"] = np.ones((50, 50), dtype=np.float64)
+    adata.uns["velocity_variances"] = np.ones((50, 50), dtype=np.float64)
+
+    return adata
 
 
 def create_kernels(adata):
