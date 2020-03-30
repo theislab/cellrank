407d8ca90fa754b74c30df8a20a4527c10739179
Michal Klein <michal.klein@tngtech.com>
2020-03-30 13:21:56 +0200

    Add more pipeline tests

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,16 +1,67 @@
 # -*- coding: utf-8 -*-
+
 import cellrank as cr
 
-
+from cellrank.tools._constants import (
+    Direction,
+    RcKey,
+    LinKey,
+    _transition,
+    _colors,
+    _lin_names,
+    _probs,
+)
 from cellrank.tools.kernels import VelocityKernel, ConnectivityKernel
+from anndata import AnnData
+
+from pandas.api.types import is_categorical_dtype
+
+
+def _assert_has_all_keys(adata: AnnData, direction: Direction):
+    assert _transition(direction) is adata.uns.keys()
+
+    if direction == Direction.FORWARD:
+        assert str(LinKey.FORWARD) in adata.obsm
+        assert isinstance(adata.obsm[str(LinKey.FORWARD)], cr.tl.Lineage)
+
+        assert _colors(LinKey.FORWARD) is adata.uns.keys()
+        assert _lin_names(LinKey.FORWARD) in adata.uns.keys()
+
+        assert str(RcKey.FORWARD) in adata.obs
+        assert is_categorical_dtype(adata.obs[str(RcKey.FORWARD)])
+
+        assert _probs(RcKey.FORWARD) in adata.obs
+    else:
+        assert str(LinKey.BACKWARD) in adata.obsm
+        assert isinstance(adata.obsm[str(LinKey.BACKWARD)], cr.tl.Lineage)
+
+        assert _colors(LinKey.BACKWARD) is adata.uns.keys()
+        assert _lin_names(LinKey.BACKWARD) in adata.uns.keys()
+
+        assert str(RcKey.BACKWARD) in adata.obs
+        assert is_categorical_dtype(adata.obs[str(RcKey.BACKWARD)])
+
+        assert _probs(RcKey.BACKWARD) in adata.obs
 
 
 class TestHighLevelPipeline:
-    pass
+    def test_fwd_pipeline(self, adata):
+        cr.tl.find_final(adata, cluster_key="clusters")
+        cr.tl.lineages(adata)
+        cr.pl.lineages(adata)
+
+        _assert_has_all_keys(adata, Direction.FORWARD)
+
+    def test_bwd_pipeline(self, adata):
+        cr.tl.find_root(adata, cluster_key="clusters")
+        cr.tl.lineages(adata)
+        cr.pl.lineages(adata)
+
+        _assert_has_all_keys(adata, Direction.BACKWARD)
 
 
 class TestLowLevelPipeline:
-    def test_default_fwd_pipelne(self, adata):
+    def test_fwd_pipelne(self, adata):
         vk = VelocityKernel(adata).compute_transition_matrix()
         ck = ConnectivityKernel(adata).compute_transition_matrix()
         final_kernel = 0.8 * vk + 0.2 * ck
@@ -33,7 +84,9 @@ class TestLowLevelPipeline:
 
         mc_fwd.compute_lineage_drivers(cluster_key="clusters", use_raw=False)
 
-    def test_default_bwd_pipelne(self, adata):
+        _assert_has_all_keys(adata, Direction.FORWARD)
+
+    def test_bwd_pipelne(self, adata):
         vk = VelocityKernel(adata, backward=True).compute_transition_matrix()
         ck = ConnectivityKernel(adata, backward=True).compute_transition_matrix()
         final_kernel = 0.8 * vk + 0.2 * ck
@@ -55,3 +108,5 @@ class TestLowLevelPipeline:
         mc_bwd.plot_lin_probs()
 
         mc_bwd.compute_lineage_drivers(cluster_key="clusters", use_raw=False)
+
+        _assert_has_all_keys(adata, Direction.BACKWARD)
