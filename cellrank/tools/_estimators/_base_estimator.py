3234d8cf937e3effe37a99ae2e94a57d573acde0
michalk8 <michal.klein@protonmail.com>
2020-04-25 18:48:58 +0200

    Move eigen to base estimator

--- cellrank/tools/_estimators/_base_estimator.py
+++ cellrank/tools/_estimators/_base_estimator.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+from scipy.sparse.linalg import eigs
+
+from cellrank.tools._utils import _eigengap
 from cellrank.tools.kernels._kernel import KernelExpression
 from typing import Optional, Dict
 
@@ -8,7 +11,6 @@ import numpy as np
 from anndata import AnnData
 from scanpy import logging as logg
 from scipy.sparse import issparse
-
 
 from cellrank.tools._constants import Direction, RcKey, LinKey, Prefix
 
@@ -70,6 +72,104 @@ class BaseEstimator(ABC):
                 "DEBUG: Reading `eig`, `approx_rcs` and `lin_probs` from `adata` object"
             )
             self._read_from_adata(g2m_key, s_key)
+
+    def _compute_eig(
+        self, k: int = 20, which: str = "LR", alpha: float = 1, only_evals: bool = False
+    ) -> None:
+        """
+        Compute eigendecomposition of transition matrix.
+
+        Uses a sparse implementation, if possible, and only computes the top k eigenvectors
+        to speed up the computation. Computes both left and right eigenvectors.
+
+        Params
+        ------
+        k
+            Number of eigenvalues/vectors to compute.
+        which
+            Eigenvalues are in general complex. `'LR'` - largest real part, `'LM'` - largest magnitude.
+        alpha
+            Used to compute the `eigengap`. :paramref:`alpha` is the weight given
+            to the deviation of an eigenvalue from one.
+
+        Returns
+        -------
+        None
+            Nothing, but updates the following fields: :paramref:`eigendecomposition`.
+        """
+
+        def get_top_k_evals():
+            return D[np.flip(np.argsort(D.real))][
+                :k
+            ]  # TODO: @Marius - I think for non-sparse matrices [:k] is necessary
+
+        def write_result(eig):
+            # write to class and AnnData object
+            if self._eig is not None:
+                logg.debug("DEBUG: Overwriting `.eig`")
+            else:
+                logg.debug(
+                    f"DEBUG: Adding `.eig` and `adata.uns['eig_{self._direction}']`"
+                )
+
+            self._eig = eig
+            self._adata.uns[f"eig_{self._direction}"] = eig
+
+        logg.info("Computing eigendecomposition of transition matrix")
+        if self._is_sparse:
+            logg.debug(f"DEBUG: Computing top `{k}` eigenvalues for sparse matrix")
+            D, V_l = eigs(self._T.T, k=k, which=which)
+            if only_evals:
+                write_result(
+                    {
+                        "D": get_top_k_evals(),
+                        "eigengap": _eigengap(D.real, alpha),
+                        "params": {"which": which, "k": k, "alpha": alpha},
+                    }
+                )
+                return
+            _, V_r = eigs(self._T, k=k, which=which)
+        else:
+            logg.warning(
+                "This transition matrix is not sparse, computing full eigendecomposition"
+            )
+            D, V_l = np.linalg.eig(self._T.T)
+            if only_evals:
+                write_result(
+                    {
+                        "D": get_top_k_evals(),
+                        "eigengap": _eigengap(D.real, alpha),
+                        "params": {"which": which, "k": k, "alpha": alpha},
+                    }
+                )
+                return
+            _, V_r = np.linalg.eig(self._T)
+
+        # Sort the eigenvalues and eigenvectors and take the real part
+        logg.debug("DEBUG: Sorting eigenvalues by their real part")
+        p = np.flip(np.argsort(D.real))
+        D, V_l, V_r = D[p], V_l[:, p], V_r[:, p]
+        e_gap = _eigengap(D.real, alpha)
+
+        write_result(
+            {
+                "D": D,
+                "V_l": V_l,
+                "V_r": V_r,
+                "eigengap": e_gap,
+                "params": {"which": which, "k": k, "alpha": alpha},
+            }
+        )
+
+    @abstractmethod
+    def compute_eig(self, k: int = 20, which: str = "LR", alpha: float = 1) -> None:
+        pass
+
+    def plot_spectrum(self):
+        pass
+
+    def plot_real_spectrum(self):
+        pass
 
     @abstractmethod
     def _read_from_adata(
