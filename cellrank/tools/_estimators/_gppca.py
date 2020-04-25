0e8bbc7106067250eac97ab366c855b95a5c1adc
michalk8 <michal.klein@protonmail.com>
2020-04-25 18:29:27 +0200

    Make read_from_adata abstract

--- cellrank/tools/_estimators/_gppca.py
+++ cellrank/tools/_estimators/_gppca.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
+from typing import Optional
+
 from cellrank.tools._estimators._base_estimator import BaseEstimator
 
 
 class GPCCA(BaseEstimator):
-    pass
+    def _read_from_adata(
+        self, g2m_key: Optional[str] = None, s_key: Optional[str] = None, **kwargs
+    ) -> None:
+        raise NotImplementedError()
+
+    def copy(self) -> "GPCCA":
+        raise NotImplementedError()
