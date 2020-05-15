fb75eeb7590bea39805549b0817b551df500a23d
Marius Lange <marius.lange@t-online.de>
2020-05-15 15:29:01 +0200

    Fix bug with use_min_chi

--- cellrank/tools/estimators/_gppca.py
+++ cellrank/tools/estimators/_gppca.py
@@ -235,7 +235,7 @@ class GPCCA(BaseEstimator):
                 minn = 3
 
             logg.debug(f"DEBUG: Calculating minChi within interval [{minn}, {maxx}]")
-            n_states = np.arange(minn, maxx)[np.argmax(gpcca.minChi(minn, maxx))]
+            n_states = int(np.arange(minn, maxx)[np.argmax(gpcca.minChi(minn, maxx))])
         elif not isinstance(n_states, int):
             raise ValueError(
                 f"Expected `n_states` to be integer when `use_min_chi=False`, found `{type(n_states).__name__}`."
