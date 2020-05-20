d885d7fbf1482892f1c46b8d236f5c1205f9ba82
Marius Lange <marius.lange@t-online.de>
2020-05-20 15:05:22 +0200

    Change defaults in transition matrix function

--- cellrank/tools/_transition_matrix.py
+++ cellrank/tools/_transition_matrix.py
@@ -14,8 +14,8 @@ def transition_matrix(
     adata: AnnData,
     vkey: str = "velocity",
     backward: bool = False,
-    weight_connectivities: Optional[float] = None,
-    sigma_corr: Optional[float] = None,
+    weight_connectivities: Optional[float] = 0.5,
+    sigma_corr: Optional[float] = 30,
     density_normalize: bool = True,
 ) -> KernelExpression:
     """
