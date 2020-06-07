ae0771401cd41d8c141dc8cb7b67b1a09858910b
michalk8 <michal.klein@protonmail.com>
2020-06-07 16:37:29 +0200

    Add eig and decomp tests - almost the same as CFLARE

---
+++ tests/test_gpcca.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+from anndata import AnnData
+
+import cellrank as cr
+from cellrank.tools.kernels import VelocityKernel, ConnectivityKernel
+from cellrank.tools._constants import StateKey, Direction
+
+
+class TestCGPCCA:
+    def test_compute_partition(self, adata_large: AnnData):
+        vk = VelocityKernel(adata_large).compute_transition_matrix()
+        ck = ConnectivityKernel(adata_large).compute_transition_matrix()
+        final_kernel = 0.8 * vk + 0.2 * ck
+
+        mc = cr.tl.GPCCA(final_kernel)
+        mc.compute_partition()
+
+        assert isinstance(mc.irreducible, bool)
+        if not mc.irreducible:
+            assert isinstance(mc.recurrent_classes, list)
+            assert isinstance(mc.transient_classes, list)
+            assert f"{StateKey.FORWARD}_rec_classes" in mc.adata.obs
+            assert f"{StateKey.FORWARD}_trans_classes" in mc.adata.obs
+        else:
+            assert mc.recurrent_classes is None
+            assert mc.transient_classes is None
+
+    def test_compute_eig(self, adata_large: AnnData):
+        vk = VelocityKernel(adata_large).compute_transition_matrix()
+        ck = ConnectivityKernel(adata_large).compute_transition_matrix()
+        final_kernel = 0.8 * vk + 0.2 * ck
+
+        mc = cr.tl.GPCCA(final_kernel)
+        mc.compute_eig(k=2)
+
+        assert isinstance(mc.eigendecomposition, dict)
+        assert set(mc.eigendecomposition.keys()) == {
+            "D",
+            "eigengap",
+            "params",
+        }
+        assert "V_l" not in mc.eigendecomposition
+        assert "V_r" not in mc.eigendecomposition
+        assert "stationary_dist" not in mc.eigendecomposition
+
+        assert f"eig_{Direction.FORWARD}" in mc.adata.uns.keys()
