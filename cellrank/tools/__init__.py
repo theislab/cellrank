94ce4af7ec7daeb1f2782670fe3c5861743d0980
Marius Lange <marius.lange@t-online.de>
2020-06-22 20:01:48 +0200

    Add high level function to compute lineage drivers

--- cellrank/tools/__init__.py
+++ cellrank/tools/__init__.py
@@ -6,5 +6,5 @@ from cellrank.tools._lineages import lineages
 from cellrank.tools.estimators import GPCCA, CFLARE
 from cellrank.tools._root_final import root_states, final_states
 from cellrank.tools._exact_mc_test import exact_mc_perm_test
-from cellrank.tools._gene_importance import gene_importance
+from cellrank.tools._gene_importance import gene_importance, lineage_drivers
 from cellrank.tools._transition_matrix import transition_matrix
