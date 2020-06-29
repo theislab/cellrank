c445b2b0c2f7a12b53a09b7ff1e1f5a4308f2d94
Marius Lange <marius.lange@t-online.de>
2020-06-29 12:21:41 +0200

    Add test for iterative solver

--- tests/test_cflare.py
+++ tests/test_cflare.py
@@ -139,26 +139,25 @@ class TestCFLARE:
         )
         np.testing.assert_allclose(mc.lineage_probabilities.X.sum(1), 1)
 
-    def test_compute_lin_probs_sparse(self, adata_large: AnnData):
+    def test_compute_lin_probs_solver(self, adata_large: AnnData):
         vk = VelocityKernel(adata_large).compute_transition_matrix()
         ck = ConnectivityKernel(adata_large).compute_transition_matrix()
         final_kernel = 0.8 * vk + 0.2 * ck
+        tol = 1e-6
 
         mc = cr.tl.CFLARE(final_kernel)
         mc.compute_eig(k=5)
         mc.compute_metastable_states(use=2)
 
-        # compute lin probs using dense routine
-        mc.compute_lin_probs()
-        l_dense = mc.lineage_probabilities
+        # compute lin probs using direct solver
+        mc.compute_lin_probs(use_iterative_solver=False)
+        l_direct = mc.lineage_probabilities
 
-        # comptue lin probs using sparse routine
-        mc.compute_lin_probs()
-        l_sparse = mc.lineage_probabilities
+        # comptue lin probs using iterative solver
+        mc.compute_lin_probs(use_iterative_solver=True, tol=tol)
+        l_iterative = mc.lineage_probabilities()
 
-        np.testing.assert_allclose(
-            l_dense.X, l_sparse.X, rtol=1e3 * EPS, atol=1e3 * EPS
-        )
+        np.testing.assert_allclose(l_direct.X, l_iterative.X, rtol=0, atol=tol)
 
     def test_compute_lineage_drivers_no_lineages(self, adata_large: AnnData):
         vk = VelocityKernel(adata_large).compute_transition_matrix()
