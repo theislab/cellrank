e8d8730d2847c76faee8cb517ee757cd1224efec
michalk8 <michal.klein@protonmail.com>
2020-07-03 22:50:16 +0200

    Fix condition

--- .scripts/travis/before_install.sh
+++ .scripts/travis/before_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     pip3 install -U pip
-elif [ "$TRAVIS_OS_NAME" == "linux" ]; then
+elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     pip install pytest-cov
 fi
--- .scripts/travis/test.sh
+++ .scripts/travis/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     python3 -m pytest
-elif [ "$TRAVIS_OS_NAME" == "linux" ]; then
+elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     pip install pytest-cov
     python -m pytest --cov-config=.coveragerc --cov=./ --cov-report=xml
 fi
