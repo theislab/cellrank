7dd3868e108f415fcc997a0b261cb03c0df1de67
michalk8 <michal.klein@protonmail.com>
2020-07-03 12:11:34 +0200

    Fix README.rst, maintainers and installation

--- cellrank/__init__.py
+++ cellrank/__init__.py
@@ -9,5 +9,6 @@ from cellrank.tools._read import read
 from cellrank.tools._constants import Lin
 
 __author__ = ", ".join(["Marius Lange", "Michal Klein", "Juan Luis Restrepo Lopez"])
+__maintainer__ = ", ".join(["Marius Lange", "Michal Klein"])
 __email__ = "info@cellrank.org"
 __version__ = "1.0.0-b.2"
