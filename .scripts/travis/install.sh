be3eec6f670675fa75cb193c2e6f5de93075ac2b
michalk8 <michal.klein@protonmail.com>
2020-07-03 22:50:16 +0200

    Fix condition

--- .scripts/travis/install.sh
+++ .scripts/travis/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     pip3 install -e".[test]"
-elif [ "$TRAVIS_OS_NAME" == "linux" ]; then
+elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     pip install -e.[test]
 fi
 
