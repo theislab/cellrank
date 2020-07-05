5bdf85b993867588bc2dc884a1d78587325737dc
michalk8 <michal.klein@protonmail.com>
2020-07-05 14:26:48 +0200

    Use virtualenv pip

--- .scripts/ci/before_install.sh
+++ .scripts/ci/before_install.sh
@@ -5,13 +5,15 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
 elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     pip install pytest-cov
     if [[ "$USE_SLEPC" == "true" ]]; then
+        pip_cmd=$(which pip)
         echo "Installing SLEPC and PETSc"
-	      sudo pip install mpi4py
 
-	      sudo pip install petsc
-	      sudo pip install petsc4py
+        sudo $pip_cmd install mpi4py
 
-	      sudo pip install slepc
-        sudo pip install slepc4py
+        sudo $pip_cmd install petsc
+        sudo $pip_cmd install petsc4py
+
+        sudo $pip_cmd install slepc
+        sudo $pip_cmd install slepc4py
     fi
 fi
