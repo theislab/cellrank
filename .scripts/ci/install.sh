677cd477e1bad189cb4c4c254d38ee21fb9024d2
michalk8 <michal.klein@protonmail.com>
2020-07-07 23:17:34 +0200

    Fix typo v2

--- .scripts/ci/install.sh
+++ .scripts/ci/install.sh
@@ -4,7 +4,7 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     pip3 install -e".[test]"
 elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     if [[ "$KRYLOV_EXTRA" == "true" ]]; then
-        pip install -e.[krylov, test]
+        pip install -e.[krylov,test]
     else
         pip install -e.[test]
     fi
