9f6f9a970dcab02ff790f512ae623e7b7f633922
michalk8 <michal.klein@protonmail.com>
2020-07-08 14:57:33 +0200

    Better CI

--- .scripts/ci/install.sh
+++ .scripts/ci/install.sh
@@ -5,8 +5,8 @@ set -e
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     pip3 install -e".[test]"
 elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    if [[ "$KRYLOV_EXTRA" == "true" ]]; then
-        pip install -e.[krylov,test] --no-cache-dir
+    if [[ "$CACHE_NAME" == "krylov" ]]; then
+        pip install -e.[krylov,test]
         python -c "import slepc; import petsc;"
         echo "Succesfully installed SLEPc and PETSc"
     else
