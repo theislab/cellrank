59bb124f31842f37e905ebae48343b919de1b703
michalk8 <michal.klein@protonmail.com>
2020-07-08 10:53:44 +0200

    Update travis scripts

--- .scripts/ci/install.sh
+++ .scripts/ci/install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+set -e
+
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     pip3 install -e".[test]"
 elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     if [[ "$KRYLOV_EXTRA" == "true" ]]; then
         pip install -e.[krylov,test]
+        python -c "import slepc; import petsc;"
+        echo "Succesfully installed SLEPc and PETSc"
     else
         pip install -e.[test]
     fi
