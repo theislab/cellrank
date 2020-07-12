539383474984beff0063dbbc8eb22555645e8346
michalk8 <michal.klein@protonmail.com>
2020-07-12 12:44:51 +0200

    Bump version: 1.0.0-b.7 → 1.0.0-b.8

--- cellrank/__init__.py
+++ cellrank/__init__.py
@@ -12,4 +12,4 @@ from cellrank.tools._constants import Lin
 __author__ = ", ".join(["Marius Lange", "Michal Klein", "Juan Luis Restrepo Lopez"])
 __maintainer__ = ", ".join(["Marius Lange", "Michal Klein"])
 __email__ = "info@cellrank.org"
-__version__ = "1.0.0-b.7"
+__version__ = "1.0.0-b.8"
