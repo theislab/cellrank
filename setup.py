f0a56ec7f1d6f395461de492b6e56327272bd921
michalk8 <michal.klein@protonmail.com>
2020-07-14 11:28:12 +0200

    Bump version: 1.0.0-b.8 → 1.0.0-rc.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
     __author__ = "Marius Lange, Michal Klein, Juan Luis Restrepo Lopez"
     __maintainer__ = "Marius Lange, Michal Klein"
     __email__ = "info@cellrank.org"
-    __version__ = "1.0.0-b.8"
+    __version__ = "1.0.0-rc.0"
 
 
 if __name__ == "__main__":
