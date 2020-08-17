c4d3d74c3e3d8d13d1f6dd444d722476883ef02d
michalk8 <michal.klein@protonmail.com>
2020-08-17 19:51:13 +0200

    Remove gene_importance, add new functions to docs

--- cellrank/tools/__init__.py
+++ cellrank/tools/__init__.py
@@ -5,6 +5,6 @@ from cellrank.tools._lineage import Lineage
 from cellrank.tools._lineages import lineages
 from cellrank.tools.estimators import GPCCA, CFLARE, BaseEstimator
 from cellrank.tools._root_final import root_states, final_states
-from cellrank.tools._lineage_drivers import gene_importance, lineage_drivers
+from cellrank.tools._lineage_drivers import lineage_drivers
 from cellrank.tools._permutation_test import _permutation_test
 from cellrank.tools._transition_matrix import transition_matrix
--- docs/source/api.rst
+++ docs/source/api.rst
@@ -32,7 +32,7 @@ Tools
     tl.root_states
     tl.final_states
     tl.lineages
-    tl.gene_importance
+    tl.lineage_drivers
     tl.transition_matrix
     tl.kernels.VelocityKernel
     tl.kernels.ConnectivityKernel
@@ -56,6 +56,7 @@ Plotting
     pl.similarity_plot
     pl.graph
     pl.lineages
+    pl.lineage_drivers
     pl.composition
 
 Utilities
