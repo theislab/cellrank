20a6b2ca406ecf326adcc1d6581a8c739f63a28a
michalk8 <michal.klein@protonmail.com>
2020-08-27 20:06:00 +0200

    Update travis deployment

--- .scripts/ci/update_notebooks_readme.sh
+++ .scripts/ci/update_notebooks_readme.sh
@@ -10,7 +10,7 @@ if [[ -n "${DEPLOY_TOKEN+x}" ]]; then
   cp ../README.rst .
 
   git add "README.rst"
-  git commit -m "[ci skip] Update README.rst"
+  git commit -m "Update README.rst"  # don't use [ci skip], we want to regenerate the notebooks
   git push "https://$DEPLOY_TOKEN@${ORIGIN:8}"
 
   cd ..
