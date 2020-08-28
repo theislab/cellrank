c74a3c767992d2fe3c2123815d6d4f760420c63d
michalk8 <michal.klein@protonmail.com>
2020-08-28 22:39:40 +0200

    Clean up travis a bit

--- .scripts/ci/before_install.sh
+++ .scripts/ci/before_install.sh
@@ -8,20 +8,4 @@ elif [[ "$TRAVIS_OS_NAME" == "linux" && "$USE_SLEPC" == "true" ]]; then
     echo "Installing APT dependencies"
     sudo apt-get update -y
     sudo apt-get install gcc gfortran libopenmpi-dev libblas-dev liblapack-dev -y
-
-    if [[ "$CACHE_NAME" != "krylov" ]]; then
-        echo "Installing SLEPc and PETSc Python3 libraries"
-        pip_cmd=$(which pip)
-
-        sudo -H $pip_cmd install mpi4py
-
-        sudo -H $pip_cmd install petsc
-        sudo -H $pip_cmd install petsc4py
-
-        sudo -H $pip_cmd install slepc
-        sudo -H $pip_cmd install slepc4py
-
-        python -c "import slepc; import petsc;"
-        echo "Successfully installed SLEPc and PETSc"
-    fi
 fi
