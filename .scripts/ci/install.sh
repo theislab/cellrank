cb7ee6ebb45a18774398dfdc163340834045d5cb
michalk8 <michal.klein@protonmail.com>
2020-08-28 22:39:40 +0200

    Clean up travis a bit

--- .scripts/ci/install.sh
+++ .scripts/ci/install.sh
@@ -5,14 +5,16 @@ set -e
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     sudo pip3 install -e".[test]"
 elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    if [[ "$CACHE_NAME" == "krylov" ]]; then
+    if [[ ! -z "${DEPLOY_TOKEN+x}" ]]; then
+        pip install pytest-cov
+        pip install codecov
+    fi
+    if [[ "$USE_SLEPC" == "true" ]]; then
         pip install -e".[krylov,test]"
         python -c "import slepc; import petsc;"
     else
         pip install -e".[test]"
     fi
-    pip install pytest-cov
-    pip install codecov
 fi
 
 python-vendorize
--- .scripts/ci/test.sh
+++ .scripts/ci/test.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    echo "Testing without coverage"
     python3 -m pytest
 elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    python -m pytest --cov-config=.coveragerc --cov=./ --cov-report=xml
-else
-    echo "Windows build is not supported"
-    exit 1
+    if [[ ! -z "${DEPLOY_TOKEN+x}" ]]; then
+        echo "Testing with coverage"
+        python -m pytest --cov-config=.coveragerc --cov=./ --cov-report=xml
+    else
+        echo "Testing without coverage"
+        python -m pytest
+    fi
 fi
