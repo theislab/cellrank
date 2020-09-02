b3d009a2e9bf43098502064d51cdc31e34c91da0
michalk8 <michal.klein@protonmail.com>
2020-09-02 17:36:12 +0200

    Fix test, improve docstring

--- cellrank/ul/_docs.py
+++ cellrank/ul/_docs.py
@@ -130,6 +130,13 @@ def inject_docs(**kwargs):  # noqa
         obj.__doc__ = dedent(obj.__doc__).format(**kwargs)
         return obj
 
+    def decorator2(obj):
+        obj.__doc__ = dedent(kwargs["__doc__"])
+        return obj
+
+    if isinstance(kwargs.get("__doc__", None), str) and len(kwargs) == 1:
+        return decorator2
+
     return decorator
 
 
