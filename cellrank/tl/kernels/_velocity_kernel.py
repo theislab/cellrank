4f1d0dc4fe9a52fa2605eba94f0227c2560c56aa
michalk8 <michal.klein@protonmail.com>
2020-09-09 23:59:04 +0200

    Add skeleton files

--- cellrank/tl/kernels/_velocity_kernel.py
+++ cellrank/tl/kernels/_velocity_kernel.py
@@ -200,8 +200,8 @@ class VelocityKernel(Kernel):
         :class:`cellrank.tl.kernels.VelocityKernel`
             Makes available the following fields:
 
-                - :paramref:`transition_matrix`.
-                - :paramref:`pearson_correlations`.
+                - :paramref:`transition_matrix`
+                - :paramref:`pearson_correlations`
 
             If ``mode={m.PROPAGATION.s!r}``, makes also available:
 
