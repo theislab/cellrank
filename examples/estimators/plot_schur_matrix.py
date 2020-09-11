4b30f1965858477b5954cd7e3416687e5d1c540b
michalk8 <michal.klein@protonmail.com>
2020-09-11 20:12:01 +0200

    Add plot Schur matrix

--- examples/estimators/plot_schur_matrix.py
+++ examples/estimators/plot_schur_matrix.py
@@ -1,5 +1,26 @@
 # -*- coding: utf-8 -*-
 """
-Dummy header
-------------
+Plot Schur matrix
+-----------------
+
+This examples show how to compute and plot Schur matrix.
 """
+
+import cellrank as cr
+
+adata = cr.datasets.pancreas_preprocessed("../example.h5ad")
+adata
+
+# %%
+# First, let us prepare the kernel using high-level pipeline and the :class:`cellrank.tl.estimators.GPCCA` estimator.
+k = cr.tl.transition_matrix(adata, show_progress_bar=False)
+g = cr.tl.estimators.GPCCA(k)
+
+# %%
+# To compute the Schur vectors, simply run the code below. Parameter ``n_components``
+# controls how many vectors to compute.
+g.compute_schur(n_components=20)
+
+# %%
+# Finally, we are ready to plot the Schur matrix. The elements in the lower triangle should all be zeros.
+g.plot_schur_matrix()
