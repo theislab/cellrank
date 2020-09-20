47a073fe68dbcf0681dea8bed075baa490c1850b
michalk8 <michal.klein@protonmail.com>
2020-09-20 15:52:06 +0200

    Update plot graph

--- examples/plotting/plot_graph.py
+++ examples/plotting/plot_graph.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Plot graph
-----------
+Plot graph structures
+---------------------
 
-This functions show how to plot graph data, such as the transition matrix.
+This functions show how to plot graph structures, such as the transition matrix.
 """
 
 import cellrank as cr
@@ -40,9 +40,9 @@ cr.pl.graph(
 )
 
 # %%
-# Below we plot only the top 3 incoming edges in descending order. Nodes are colored in by the sum of incoming weights,
-# in this case, the transition probabilities. Note that the aggregate values are calculated before any filtering
-# or normalization takes place.
+# Below we plot at most the top 3 incoming edges restricted to ``ixs`` in descending order. Nodes are also colored in
+# by the sum of incoming weights, in this case, the transition probabilities. Note that the aggregate values are
+# calculated before any filtering or normalization takes place.
 cr.pl.graph(
     adata,
     "T_fwd",
@@ -53,16 +53,39 @@ cr.pl.graph(
 )
 
 # %%
-# Lastly, we can visualize different edge aggregations, such as minimum or maximum. Here we take the top 5 outgoing
-# edges for each node in descending order and color the nodes by the maximum outgoing weights.
+# Lastly, we can visualize different edge aggregations, such as minimum or maximum. Here we take at most 5 outgoing
+# edges restricted to ``ixs`` for each node in descending order and color the nodes by the maximum outgoing weights.
+#
+# Here we also specify ``edge_reductions_restrict_to_ixs`` (by default, it is the same as ``ixs``) that computes the
+# statistic between the cells marked with ``ixs`` and ``edge_reduction_indices``.
+#
+# Below we compare the maximum transition from each of the `"Delta"` cells to any of the `"Alpha"` cells or the `"Beta"`
+# cells. We can visually inspect that the values are larger when going to the `"Beta"` cells, which is consistent with
+# known biology, as well as our prediction, see .
 cr.pl.graph(
     adata,
     "T_fwd",
     ixs=ixs,
     edge_alpha=0.5,
     node_size=200,
-    keys=("outgoing",),
+    keys="outgoing",
     show_arrows=False,
     top_n_edges=(5, False, "outgoing"),
+    title="outgoing to Beta",
     edge_reductions=np.max,
+    edge_reductions_restrict_to_ixs=np.where(adata.obs["clusters"] == "Beta")[0],
+)
+
+cr.pl.graph(
+    adata,
+    "T_fwd",
+    ixs=ixs,
+    edge_alpha=0.5,
+    node_size=200,
+    keys="outgoing",
+    show_arrows=False,
+    top_n_edges=(5, False, "outgoing"),
+    title="outgoing to Alpha",
+    edge_reductions=np.max,
+    edge_reductions_restrict_to_ixs=np.where(adata.obs["clusters"] == "Alpha")[0],
 )
