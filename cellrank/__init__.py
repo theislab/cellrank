0beaf36f82ab152fd1e66dee1f535978e504b6a9
michalk8 <michal.klein@protonmail.com>
2020-10-15 12:31:21 +0200

    Add __full_version__

--- cellrank/__init__.py
+++ cellrank/__init__.py
@@ -12,3 +12,11 @@ __author__ = ", ".join(["Marius Lange", "Michal Klein", "Juan Luis Restrepo Lope
 __maintainer__ = ", ".join(["Marius Lange", "Michal Klein"])
 __version__ = "1.0.0-rc.12"
 __email__ = "info@cellrank.org"
+
+try:
+    from importlib_metadata import version  # Python < 3.8
+except ImportError:
+    from importlib.metadata import version  # Python = 3.8
+__full_version__ = version(__name__)
+
+del version
