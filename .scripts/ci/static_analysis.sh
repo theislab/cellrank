b706235973caaddd6013ae1d8d74735b91e0c14c
michalk8 <michal.klein@protonmail.com>
2020-10-16 12:01:41 +0200

    Enable numba, allow failures for 3.9-dev

--- .scripts/ci/static_analysis.sh
+++ .scripts/ci/static_analysis.sh
@@ -2,7 +2,9 @@
 
 set -ev
 
+sudo apt-get update -y
+sudo apt-get install pandoc
 pip install -e".[docs,dev]"
-# this doesn't show which things would be blackened, but it should be fine
+
 pre-commit run --all-files
 sphinx-build -j2 -b linkcheck docs/source docs/build/linkcheck
