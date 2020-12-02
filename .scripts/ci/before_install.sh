27d1b3fbe5624431171e71e7c2c21ef75661d924
michalk8 <michal.klein@protonmail.com>
2020-12-02 23:23:14 +0100

    Fix sourceline, temp disable linkcheck

--- .scripts/ci/before_install.sh
+++ .scripts/ci/before_install.sh
@@ -5,8 +5,8 @@ set -ev
 if [[ "$OS" == "ubuntu-latest" ]]; then
     echo "Installing APT dependencies"
     # R-related
-    sudo apt-key adv --recv-keys --keyserver "http://keyserver.ubuntu.com:11371/pks/lookup?op=get&search=0xE298A3A825C0D65DFD57CBB651716619E084DAB9"
-    sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/"
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+    sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
 
     sudo apt-get update -y
     sudo apt-get install libopenblas-base r-base r-base-dev r-cran-mgcv -y
