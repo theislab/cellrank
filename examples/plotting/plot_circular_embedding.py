00c18efe21bbb6fc52a731f8bcfdf641e63eded6
michalk8 <michal.klein@protonmail.com>
2020-12-21 14:15:24 +0100

    [ci skip] Add example

---
+++ examples/plotting/plot_circular_embedding.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Plot circular embedding
+-----------------------
+
+This example shows how to plot absorption probabilities using circular aposteriori projection as used in [Velten17]_.
+"""
+
+import cellrank as cr
+
+adata = cr.datasets.pancreas_preprocessed("../example.h5ad")
+adata
+
+# %%
+# First, we compute the absorption probabilities.
+cr.tl.terminal_states(
+    adata,
+    cluster_key="clusters",
+    weight_connectivities=0.2,
+    n_states=3,
+    softmax_scale=4,
+    show_progress_bar=False,
+)
+cr.tl.lineages(adata)
+
+# %%
+# We can now visualize the absorption probabilities by projecting them onto a unit circle. The tips of the simplex
+# indicate the probability of 1.0 of belonging to the lineage and the midpoint between the edges signifies where the
+# probabilities of belonging to the connected lineages are 0.5.
+cr.pl.circular_projection(
+    adata, keys=["clusters", "to_terminal_states_dp"], legend_loc="upper right"
+)
