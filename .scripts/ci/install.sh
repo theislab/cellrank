c05b11e7ab97960d73479af40d276117e1808187
michalk8 <michal.klein@protonmail.com>
2021-02-03 11:01:38 +0100

    Try legacy resolver

--- .scripts/ci/install.sh
+++ .scripts/ci/install.sh
@@ -2,19 +2,19 @@
 
 set -ev
 
-python -m pip install --upgrade pip
+python -m pip install --upgrade "pip<21.0.0" --use-deprecated=legacy-resolver
 pip install pytest-cov codecov jax jaxlib
 
 if [[ "$OS" == "macos-latest" ]]; then
-  pip install -e".[test]"
+  pip install -e".[test]" --use-deprecated=legacy-resolver
 elif [[ "$OS" == "ubuntu-latest" ]]; then
   if [[ "$USE_SLEPC" == "true" ]]; then
       pip install -e".[krylov,test]"
       python -c "import slepc; import petsc;"
   else
-      pip install -e".[test]"
+      pip install -e".[test]" --use-deprecated=legacy-resolver
   fi
-  pip install rpy2>=3.3.0
+  pip install rpy2>=3.3.0 --use-deprecated=legacy-resolver
   Rscript --vanilla -e "library('mgcv')"
 else
   exit 42
