a823f76fe40c23c10341e687efc8139da679ed6e
michalk8 <46717574+michalk8@users.noreply.github.com>
2021-03-13 19:11:28 +0100

    Smoothing for Pseudotime kernel (#514)
    * Rename PalantirKernel -> PseudotimeKernel
    
    * Add initial abstraction
    
    * Add Palantir scheme
    
    * Add soft PT
    
    * Remove old test
    
    * Fix wrong condition
    
    * Update docs and defaults
    
    * Fix Pseudotime kernel docstring
    
    * Fix more docstrings
    
    * Update docs
    
    * Add tests, pin down numba
    
    Co-authored-by: Marius Lange <marius.lange@t-online.de>

--- cellrank/tl/kernels/_pseudotime_kernel.py
+++ cellrank/tl/kernels/_pseudotime_kernel.py
@@ -1,33 +1,38 @@
-"""Palantir kernel module."""
+"""Pseudotime kernel module."""
 from copy import copy
+from typing import Any, Union, Callable, Optional
+
+from typing_extensions import Literal
 
 import numpy as np
 
 from cellrank import logging as logg
 from cellrank.ul._docs import d
-from cellrank.tl._utils import _bias_knn, _connected
+from cellrank.tl._utils import _connected
 from cellrank.tl.kernels import Kernel
-from cellrank.tl._constants import Direction
+from cellrank.tl._constants import Direction, ThresholdScheme
 from cellrank.tl.kernels._base_kernel import (
     _LOG_USING_CACHE,
     _ERROR_EMPTY_CACHE_MSG,
     AnnData,
     _dtype,
 )
+from cellrank.tl.kernels._pseudotime_schemes import (
+    ThresholdSchemeABC,
+    HardThresholdScheme,
+    SoftThresholdScheme,
+    CustomThresholdScheme,
+)
 
 
 @d.dedent
-class PalantirKernel(Kernel):
+class PseudotimeKernel(Kernel):
     """
-    Kernel which computes transition probabilities in a similar way to *Palantir*, see [Setty19]_.
+    Kernel which computes directed transition probabilities based on a KNN graph and pseudotime.
 
-    *Palantir* computes a KNN graph in gene expression space and a pseudotime, which it then uses to direct the edges of
-    the KNN graph, such that they are more likely to point into the direction of increasing pseudotime. To avoid
-    disconnecting the graph, it does not remove all edges that point into the direction of decreasing pseudotime
-    but keeps the ones that point to nodes inside a close radius. This radius is chosen according to the local density.
-
-    The implementation presented here won't exactly reproduce the original *Palantir* algorithm (see below)
-    but the results are qualitatively very similar.
+    The KNN graph contains information about the (undirected) connectivities among cells, reflecting their similarity.
+    Pseudotime can be used to either remove edges that point against the direction of increasing pseudotime (see
+    [Setty19]_, or to downweight them (see [VIA21]_).
 
     %(density_correction)s
 
@@ -74,30 +79,40 @@ class PalantirKernel(Kernel):
         logg.debug("Clipping the pseudotime to 0-1 range")
         self._pseudotime = np.clip(self._pseudotime, 0, 1)
 
+    @d.dedent
     def compute_transition_matrix(
         self,
+        threshold_scheme: Union[Literal["soft", "hard"], Callable] = "hard",
         k: int = 3,
+        b: float = 20.0,
+        nu: float = 1.0,
+        percentile: Optional[int] = 95,
         density_normalize: bool = True,
         check_irreducibility: bool = False,
-    ) -> "PalantirKernel":
+        **kwargs: Any,
+    ) -> "PseudotimeKernel":
         """
         Compute transition matrix based on KNN graph and pseudotemporal ordering.
 
-        This is a re-implementation of the Palantir algorithm by [Setty19]_.
-        Note that this won't exactly reproduce the original Palantir results, for three reasons:
+        Depending on the choice of the `thresholding_scheme`, this is based on ideas by either Palantir (see [Setty19]_)
+        or VIA (see [VIA21]_).
 
-            - Palantir computes the KNN graph in a scaled space of diffusion components.
-            - Palantir uses its own pseudotime to bias the KNN graph which is not implemented here.
-            - Palantir uses a slightly different mechanism to ensure the graph remains connected when removing edges
-              that point into the "pseudotime past".
+        When using a `'hard'` thresholding scheme, this based on ideas by *Palantir* (see [Setty19]_) which removes some
+        edges that point against the direction of increasing pseudotime. To avoid disconnecting the graph, it does not
+        remove all edges that point against the direction of increasing pseudotime but keeps the ones that point to
+        cells inside a close radius. This radius is chosen according to the local cell density.
 
-        If you would like to reproduce the original results, please use the original Palantir algorithm.
+        When using a `'soft'` thresholding scheme, this is based on ideas by *VIA* (see [VIA21]_) which downweights
+        edges that points against the direction of increasing pseudotime. Essentially, the further "behind" a query
+        cell is in pseudotime with respect to the current reference cell, the more penalized will be its
+        graph-connectivity.
 
         Parameters
         ----------
         k
             Number of neighbors to keep for each node, regardless of pseudotime.
-            This is done to ensure that the graph remains connected.
+            This is done to ensure that the graph remains connected. Only used when `threshold_scheme='hard'`.
+        %(soft_scheme_kernel)s
         density_normalize
             Whether or not to use the underlying KNN graph for density normalization.
         check_irreducibility
@@ -105,11 +120,10 @@ class PalantirKernel(Kernel):
 
         Returns
         -------
-        :class:`cellrank.tl.kernels.PalantirKernel`
+        :class:`cellrank.tl.kernels.PseudotimeKernel`
             Makes :paramref:`transition_matrix` available.
         """
-
-        start = logg.info("Computing transition matrix based on Palantir-like kernel")
+        start = logg.info("Computing transition matrix based on pseudotime")
 
         # get the connectivities and number of neighbors
         n_neighbors = (
@@ -123,7 +137,32 @@ class PalantirKernel(Kernel):
             )
             n_neighbors = np.min(self._conn.sum(1))
 
-        params = dict(k=k, dnorm=density_normalize, n_neighs=n_neighbors)  # noqa
+        if isinstance(threshold_scheme, str):
+            threshold_scheme = ThresholdScheme(threshold_scheme)
+            if threshold_scheme == ThresholdScheme.SOFT:
+                scheme = SoftThresholdScheme()
+                kwargs["b"], kwargs["nu"] = b, nu
+            elif threshold_scheme == ThresholdScheme.HARD:
+                scheme = HardThresholdScheme()
+                kwargs["k"], kwargs["n_neighs"] = k, n_neighbors
+            else:
+                raise NotImplementedError(
+                    f"Threshold scheme `{threshold_scheme}` is not yet implemented."
+                )
+        elif isinstance(threshold_scheme, ThresholdSchemeABC):
+            pass
+        elif callable(threshold_scheme):
+            scheme = CustomThresholdScheme(threshold_scheme)
+        else:
+            raise TypeError(
+                f"Expected `threshold_scheme` to be either a `str` or a `callable`, found `{type(threshold_scheme)}`."
+            )
+
+        params = {
+            "dnorm": density_normalize,
+            "scheme": str(threshold_scheme),
+            **kwargs,
+        }
         if params == self._params:
             assert self.transition_matrix is not None, _ERROR_EMPTY_CACHE_MSG
             logg.debug(_LOG_USING_CACHE)
@@ -134,13 +173,15 @@ class PalantirKernel(Kernel):
 
         # handle backward case and run biasing function
         pseudotime = (
-            np.max(self.pseudotime) - self.pseudotime
+            np.nanmax(self.pseudotime) - self.pseudotime
             if self._direction == Direction.BACKWARD
             else self.pseudotime
         )
-        biased_conn = _bias_knn(
-            conn=self._conn, pseudotime=pseudotime, n_neighbors=n_neighbors, k=k
-        ).astype(_dtype)
+
+        biased_conn = scheme.bias_knn(self._conn.copy(), pseudotime, **kwargs).astype(
+            _dtype
+        )
+
         # make sure the biased graph is still connected
         if not _connected(biased_conn):
             logg.warning("Biased KNN graph is disconnected")
@@ -150,7 +191,6 @@ class PalantirKernel(Kernel):
             density_normalize=density_normalize,
             check_irreducibility=check_irreducibility,
         )
-
         logg.info("    Finish", time=start)
 
         return self
@@ -160,9 +200,11 @@ class PalantirKernel(Kernel):
         """Pseudotemporal ordering of cells."""
         return self._pseudotime
 
-    def copy(self) -> "PalantirKernel":
+    def copy(self) -> "PseudotimeKernel":
         """Return a copy of self."""
-        pk = PalantirKernel(self.adata, backward=self.backward, time_key=self._time_key)
+        pk = PseudotimeKernel(
+            self.adata, backward=self.backward, time_key=self._time_key
+        )
         pk._pseudotime = copy(self.pseudotime)
         pk._params = copy(self._params)
         pk._cond_num = self.condition_number
@@ -170,7 +212,7 @@ class PalantirKernel(Kernel):
 
         return pk
 
-    def __invert__(self) -> "PalantirKernel":
+    def __invert__(self) -> "PseudotimeKernel":
         super().__invert__()
-        self._pseudotime = 1 - self.pseudotime
+        self._pseudotime = np.nanmax(self.pseudotime) - self.pseudotime
         return self
