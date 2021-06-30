4bcaebdbee104104ab5d11f86e3ef54b02163893
Michal Klein <michal.klein@protonmail.com>
2021-06-30 11:24:26 +0200

    Pin numba in reqs again

--- .scripts/ci/install.sh
+++ .scripts/ci/install.sh
@@ -24,4 +24,4 @@ fi
 # TODO: remove once scVelo unpins umap-learn
 # ensure correct umap-learn version is installed, see:
 # https://github.com/bioconda/bioconda-recipes/pull/28591
-pip install "umap-learn>=0.5.1" adjustText
+pip install "umap-learn>=0.5.1" "numba>=0.53.1" adjustText
