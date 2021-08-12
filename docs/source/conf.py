a5874eae55a329c3475062a1a9fd72853209dcb5
Michal Klein <michal.klein@protonmail.com>
2021-08-12 19:31:53 +0200

    [ci skip] Fix pet peeve

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -270,7 +270,7 @@ todo_include_todos = False
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
-html_theme_options = dict(navigation_depth=4, logo_only=True)
+html_theme_options = {"navigation_depth": 4, "logo_only": True}
 html_show_sphinx = False
 html_show_sourcelink = False
 
