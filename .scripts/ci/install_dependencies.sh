fa580842544300bbf89978e28c006ec9d2918311
michalk8 <46717574+michalk8@users.noreply.github.com>
2022-04-07 18:06:03 +0000

    Fix PETSc/SLEPc on the CI for Linux (#850)
    * Try fix PETSc/SLEPc on the CI for Linux
    
    * Fix typo in CI .yml file
    
    * Fix typo in OS check
    
    * Fix typo v2
    
    * Change file permission
    
    * Different way of exporting PETSc/SLEPc/ env vars
    
    * Fix tox.ini
    
    * Export PETSc/SLEPc env variables
    
    * Try whether symlink is necessary
    
    * Use py37 for external only
    
    * Try pinning numpy for SLEPc CI
    
    * Revert krylov -> slepc

--- .scripts/ci/install_dependencies.sh
+++ .scripts/ci/install_dependencies.sh
@@ -1,23 +1,15 @@
 #!/usr/bin/env bash
 
-set -ev
+set -euo pipefail
 
-if [[ "$RUNNER_OS" == "Linux" ]]; then
-    echo "Installing APT dependencies"
-    # https://github.com/yarnpkg/yarn/issues/7866
-    curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-    # R-related
-    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-    sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+echo "Fetching Yarn GPG key"
+# https://github.com/yarnpkg/yarn/issues/7866
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 
-    sudo apt update -y
-    sudo apt install libopenblas-base r-base r-base-dev r-cran-mgcv -y
+echo "Installing R"
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
 
-    if [[ "$USE_SLEPC" == "true" ]]; then
-        echo "Installing PETSc/SLEPc dependencies"
-        sudo apt install gcc gfortran libopenmpi-dev libblas-dev liblapack-dev petsc-dev slepc-dev -y
-    fi
-elif [[ "$RUNNER_OS" != "macOS" ]]; then
-    echo "Invalid OS for dependencie installations: $OS"
-    exit 42
-fi
+echo "Installing APT dependencies"
+sudo apt update -y
+sudo apt install libopenblas-base r-base r-base-dev r-cran-mgcv -y
