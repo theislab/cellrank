1a06ed65e2ac46d4264c67c095c1b65b0770d53e
Michal Klein <michal.klein@protonmail.com>
2022-04-07 15:06:12 +0200

    Try whether symlink is necessary

--- .scripts/ci/install_petsc_linux.sh
+++ .scripts/ci/install_petsc_linux.sh
@@ -33,10 +33,10 @@ install_petsc
 install_slepc
 popd
 
-echo "Installing numpy"
-python -m pip install --upgrade pip
-pip install numpy
+# echo "Installing numpy"
+# python -m pip install --upgrade pip
+# pip install "numpy<1.22"
 
-echo "Symlinking numpy"
-NUMPY_INCLUDE="$(python -c 'import numpy; print(numpy.get_include())')"
-ln -sfv "$NUMPY_INCLUDE/numpy" "$PETSC_DIR/$PETSC_ARCH/include"
+# echo "Symlinking numpy"
+# NUMPY_INCLUDE="$(python -c 'import numpy; print(numpy.get_include())')"
+# ln -sfv "$NUMPY_INCLUDE/numpy" "$PETSC_DIR/$PETSC_ARCH/include"
