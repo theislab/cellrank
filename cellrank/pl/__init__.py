56b3bd1ea7358409e7cd97b368a5e4a217a1f6f2
Michal Klein <michal.klein@protonmail.com>
2022-06-03 21:05:15 +0200

    Remove cellrank.pl.{initial,terminal}_states

--- cellrank/pl/__init__.py
+++ cellrank/pl/__init__.py
@@ -4,5 +4,4 @@ from cellrank.pl._log_odds import log_odds
 from cellrank.pl._gene_trend import gene_trends
 from cellrank.pl._cluster_fates import cluster_fates
 from cellrank.pl._cluster_lineage import cluster_lineage
-from cellrank.pl._init_term_states import initial_states, terminal_states
 from cellrank.pl._circular_projection import circular_projection
