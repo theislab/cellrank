7b648f074714c498dc17ab4be71cf28838b6d5b1
michalk8 <46717574+michalk8@users.noreply.github.com>
2022-06-10 09:38:31 +0000

    Feature/bone marrow data (#886)
    * Add cellrank.tl.bone_marrow
    
    * Update docstring
    
    * [ci skip] Add bone marrow dataset to docs

--- cellrank/datasets/__init__.py
+++ cellrank/datasets/__init__.py
@@ -2,6 +2,7 @@ from cellrank.datasets._datasets import (
     lung,
     pancreas,
     zebrafish,
+    bone_marrow,
     reprogramming_morris,
     pancreas_preprocessed,
     reprogramming_schiebinger,
