ab95abb862aa73054b93ecffe00676d1352582a3
Soeren Becker <soren.a.becker@gmail.com>
2023-04-03 11:24:36 +0200

    deleted needless comment

--- cellrank/kernels/__init__.py
+++ cellrank/kernels/__init__.py
@@ -1,6 +1,6 @@
 from cellrank.kernels import utils
 from cellrank.kernels._base_kernel import Kernel
-from cellrank.kernels._monge_kernel import MongeKernel  # depends on VelocityKernel
+from cellrank.kernels._monge_kernel import MongeKernel
 from cellrank.kernels._velocity_kernel import VelocityKernel
 from cellrank.kernels._cytotrace_kernel import CytoTRACEKernel
 from cellrank.kernels._pseudotime_kernel import PseudotimeKernel
