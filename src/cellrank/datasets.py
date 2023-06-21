850057c8e403067be3fccf5a6e9dbcf285d9b207
Marius Lange <marius.lange@t-online.de>
2023-06-21 18:14:14 +0200

    Minor changes

--- src/cellrank/datasets.py
+++ src/cellrank/datasets.py
@@ -32,7 +32,7 @@ class ReprogrammingSubset(ModeEnum):
 _datasets = {
     "pancreas": ("https://figshare.com/ndownloader/files/25060877", (2531, 27998)),
     "pancreas_preprocessed": ("https://figshare.com/ndownloader/files/25030028", (2531, 2000)),
-    "pancreas_preprocesses_vk": ("https://figshare.com/ndownloader/files/41325411", (2531, 5974)),
+    "pancreas_preprocessed_vk": ("https://figshare.com/ndownloader/files/41325411", (2531, 5974)),
     "lung": ("https://figshare.com/ndownloader/files/25038224", (24882, 24051)),
     "reprogramming_morris": ("https://figshare.com/ndownloader/files/25503773", (104679, 22630)),
     "zebrafish": ("https://figshare.com/ndownloader/files/27265280", (2434, 23974)),
