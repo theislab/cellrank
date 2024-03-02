7b1d5b9b9ab810c3a2b4cb5dfadb70abfea87f38
Philipp Weiler <weiler.philipp@gmail.com>
2024-03-02 15:00:40 +0000

    Add `test_plotting.py::TestGPCCA::test_plot_tsi`

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -2257,6 +2257,13 @@ class TestGPCCA:
             save=fpath,
         )
 
+    @compare(kind="gpcca")
+    def test_plot_tsi(self, mc: GPCCA, fpath: str):
+        terminal_states = ["Neuroblast", "Astrocyte", "Granule mature"]
+        cluster_key = "clusters"
+        _ = mc.tsi(n_macrostates=3, terminal_states=terminal_states, cluster_key=cluster_key, n_cells=10)
+        mc.plot_tsi(dpi=DPI, save=fpath)
+
 
 class TestLineage:
     @compare(kind="lineage")
